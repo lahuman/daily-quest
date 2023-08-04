facd07437f4cac9becf3d50e440f53fd213d88de
Daniel Lim <lahuman@daum.net>
2023-08-04 11:30:38 +0900

    fix : 엔터 입력시 2개씩 퀘스트 등록 오류 수정 처리

--- web/app/todo/page.tsx
+++ web/app/todo/page.tsx
@@ -6,10 +6,9 @@ import { TodoVo } from "./TodoVo";
 import { client } from "./fetchHelper";
 
 enum TODO_TYPE {
-  T = 'T',
-  DT = 'DT',
+  T = "T",
+  DT = "DT",
 }
-
 
 export default function Todo() {
   const [error, setError] = useState("");
@@ -33,46 +32,47 @@ export default function Todo() {
   function changeDate(isNext: boolean) {
     const fun = isNext ? addDays : subDays;
 
-     setDateStr(
-      format(
-        fun(parse(dateStr, "yyyyMMdd", new Date()), 1),
-        "yyyyMMdd"
-      )
-    )
+    setDateStr(
+      format(fun(parse(dateStr, "yyyyMMdd", new Date()), 1), "yyyyMMdd")
+    );
   }
 
-  function saveNewTodo(){
+  function saveNewTodo() {
     setLoading(true);
     client("/todo", {
       method: "POST",
       body: {
-        type, 
+        type,
         content: newTodo,
-        todoDay: dateStr
-      }
-    }).then(r => {
-      setNewTodo("");
-      setLoading(false);
-      getTodoList();
-    }).catch(e => {
-      alert(e);
+        todoDay: dateStr,
+      },
     })
+      .then((r) => {
+        setNewTodo("");
+        setLoading(false);
+        getTodoList();
+      })
+      .catch((e) => {
+        alert(e);
+      });
   }
 
-  function updateTodoCompleted(row: TodoVo){
+  function updateTodoCompleted(row: TodoVo) {
     setLoading(true);
 
     client("/todo", {
       method: "PUT",
       body: {
-        ...row
-      }
-    }).then(r => {
-      setLoading(false);
-      getTodoList();
-    }).catch(e => {
-      alert(e);
-    });
+        ...row,
+      },
+    })
+      .then((r) => {
+        setLoading(false);
+        getTodoList();
+      })
+      .catch((e) => {
+        alert(e);
+      });
   }
 
   function deleteTodo(row: TodoVo) {
@@ -81,80 +81,127 @@ export default function Todo() {
     client("/todo", {
       method: "DELETE",
       body: {
-        ...row
-      }
-    }).then(r => {
-      setLoading(false);
-      getTodoList();
-    }).catch(e => {
-      alert(e);
+        ...row,
+      },
     })
+      .then((r) => {
+        setLoading(false);
+        getTodoList();
+      })
+      .catch((e) => {
+        alert(e);
+      });
   }
   return (
     <>
-    {loading && <div className="fixed top-0 left-0 right-0 bottom-0 w-full h-screen z-50 overflow-hidden bg-gray-700 opacity-75 flex flex-col items-center justify-center">
-    <svg
-      className="animate-spin h-8 w-8 text-gray-800"
-      xmlns="http://www.w3.org/2000/svg"
-      fill="none"
-      viewBox="0 0 24 24"
-    >
-      <circle
-        className="opacity-25"
-        cx="12"
-        cy="12"
-        r="10"
-        stroke="currentColor"
-        strokeWidth="5"
-      ></circle>
-      <path
-        className="opacity-75"
-        fill="currentColor"
-        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-      ></path>
-              </svg><h2 className="text-center text-white text-xl font-semibold">Loading...</h2>
-      
-      <p className="w-1/3 text-center text-white">This may take a few seconds, please don&apos;t close this page.</p>
-    </div>}
+      {loading && (
+        <div className="fixed top-0 left-0 right-0 bottom-0 w-full h-screen z-50 overflow-hidden bg-gray-700 opacity-75 flex flex-col items-center justify-center">
+          <svg
+            className="animate-spin h-8 w-8 text-gray-800"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <circle
+              className="opacity-25"
+              cx="12"
+              cy="12"
+              r="10"
+              stroke="currentColor"
+              strokeWidth="5"
+            ></circle>
+            <path
+              className="opacity-75"
+              fill="currentColor"
+              d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+            ></path>
+          </svg>
+          <h2 className="text-center text-white text-xl font-semibold">
+            Loading...
+          </h2>
 
-    {/* bg-fixed bg-center bg-cover bg-no-repeat bg-[url('https://lahuman.github.io/assets/img/logo.png')] */}
+          <p className="w-1/3 text-center text-white">
+            This may take a few seconds, please don&apos;t close this page.
+          </p>
+        </div>
+      )}
+
+      {/* bg-fixed bg-center bg-cover bg-no-repeat bg-[url('https://lahuman.github.io/assets/img/logo.png')] */}
       <div className="justify-center h-screen  ">
         <div className="w-full px-4 py-8 mx-auto shadow lg:w-1/3">
           <div className="flex justify-center  mb-6">
             <h1 className="text-2xl font-bold text-purple-600">
               {" "}
-              <button type="button" onClick={(e) => changeDate(false)}  className="bg-purple-300 text-white rounded-l-md border-r border-gray-100 py-2 hover:bg-purple-400 hover:text-white px-3">
-              <div className="flex flex-row align-middle">
-                <svg className="w-5 mr-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                  <path fill-rule="evenodd" d="M7.707 14.707a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l2.293 2.293a1 1 0 010 1.414z" clip-rule="evenodd"></path>
-                </svg>
-                <p className="ml-2"></p>
-              </div>
-            </button>
-           {" "}
+              <button
+                type="button"
+                onClick={(e) => changeDate(false)}
+                className="bg-purple-300 text-white rounded-l-md border-r border-gray-100 py-2 hover:bg-purple-400 hover:text-white px-3"
+              >
+                <div className="flex flex-row align-middle">
+                  <svg
+                    className="w-5 mr-2"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M7.707 14.707a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 1.414L5.414 9H17a1 1 0 110 2H5.414l2.293 2.293a1 1 0 010 1.414z"
+                      clip-rule="evenodd"
+                    ></path>
+                  </svg>
+                  <p className="ml-2"></p>
+                </div>
+              </button>{" "}
               {format(parse(dateStr, "yyyyMMdd", new Date()), "yyyy-MM-dd EEE")}{" "}
-              <button type="button" onClick={(e) => changeDate(true)}  className="bg-purple-300 text-white rounded-r-md py-2 border-l border-gray-200 hover:bg-purple-400 hover:text-white px-3">
-              <div className="flex flex-row align-middle">
-                <span className="mr-2"></span>
-                <svg className="w-5 ml-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                  <path fill-rule="evenodd" d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z" clip-rule="evenodd"></path>
-                </svg>
-              </div>
-            </button>
+              <button
+                type="button"
+                onClick={(e) => changeDate(true)}
+                className="bg-purple-300 text-white rounded-r-md py-2 border-l border-gray-200 hover:bg-purple-400 hover:text-white px-3"
+              >
+                <div className="flex flex-row align-middle">
+                  <span className="mr-2"></span>
+                  <svg
+                    className="w-5 ml-2"
+                    fill="currentColor"
+                    viewBox="0 0 20 20"
+                    xmlns="http://www.w3.org/2000/svg"
+                  >
+                    <path
+                      fill-rule="evenodd"
+                      d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z"
+                      clip-rule="evenodd"
+                    ></path>
+                  </svg>
+                </div>
+              </button>
             </h1>
           </div>
           <div className="flex ">
-            <select className="w-2/12 px-2 py-3 border rounded outline-none border-grey-600 mr-2" value={type} onChange={e => setType(e.target.value === "DT"? TODO_TYPE.DT : TODO_TYPE.T)}>
+            <select
+              className="w-2/12 px-2 py-3 border rounded outline-none border-grey-600 mr-2"
+              value={type}
+              onChange={(e) =>
+                setType(e.target.value === "DT" ? TODO_TYPE.DT : TODO_TYPE.T)
+              }
+            >
               <option value="DT">Repeat</option>
               <option value="T">Once</option>
             </select>
             <input
               value={newTodo}
-              onChange={e => setNewTodo(e.target.value)}
-              onKeyDown={e => {
-                if( e.key === 'Enter' && newTodo.trim() !== "") {
+              onChange={(e) => setNewTodo(e.target.value)}
+              onKeyDown={(e) => {
+                if (
+                  e.key === "Enter" &&
+                  e.nativeEvent.isComposing === false &&
+                  newTodo.trim() !== ""
+                ) {
+                  console.log("엔터 팍팍!!");
+                  e.preventDefault();
                   saveNewTodo();
-                }}}
+                }
+              }}
               type="text"
               placeholder="Write a Quest and hit Enter."
               className="w-9/12 px-2 py-3 border rounded outline-none border-grey-600"
@@ -173,7 +220,10 @@ export default function Todo() {
                       type="checkbox"
                       id={`todo${idx}`}
                       onChange={({ target: { checked } }) => {
-                        updateTodoCompleted({...todo, completeYn : checked ? "Y" : "N"});
+                        updateTodoCompleted({
+                          ...todo,
+                          completeYn: checked ? "Y" : "N",
+                        });
                       }}
                       checked={todo.completeYn === "Y"}
                     />
@@ -189,7 +239,7 @@ export default function Todo() {
                   <button
                     type="button"
                     className="absolute right-0 flex items-center"
-                    onClick={e => deleteTodo(todo)}
+                    onClick={(e) => deleteTodo(todo)}
                   >
                     <svg
                       xmlns="http://www.w3.org/2000/svg"
